665cac75c51e0e893032ed4c74a47f5d24335a17
Patricio Muriega <pmuriega.tc@gmail.com>
2022-01-27 12:19:51 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <nav class="nav">
+          <img src="../assets/chandler.jpg" alt="chandler" class="nav__img">
+
+          <h2 class="nav__title">Could it be any shorter?</h2>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .nav {
+        display: flex;
+        margin: 0 0 15px 0;
+        width: 100%;
+        align-items: center;
+        background-color: #822E81;
+    }
+    .nav__title {
+        color: #F0F2A6;
+        margin-left: 10px;
+    }
+    .nav__img {
+        width: 10%;
+
+    }
+</style>
